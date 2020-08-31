c2ce88e94aefc35350691d62e4e1a3cf32091db3
pacobbpsk <66210873+pacobbpsk@users.noreply.github.com>
2020-08-31 16:11:38 +0900

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -2,14 +2,32 @@ $(function(){
   $('#menu-icon').click(function(){
     $('#menu-contents').fadeIn();
     $('.upper').fadeOut();
+    $('body,html').addClass('modal-fix');
 
   });
   $('#close-btn').click(function(){
     $('#menu-contents').fadeOut();
     $('.upper').fadeIn();
+    $('body,html').removeClass('modal-fix');
   });
  $('.menu-list').click(function(){
    $('#menu-contents').fadeOut();
    $('.upper').fadeIn();
+ });
+
+ var topPage = $('#pageTop');
+ topPage.hide();
+ $(window).scroll(function(){
+   if ($(this).scrollTop() > 100){
+     topPage.fadeIn();
+   } else {
+     topPage.fadeOut();
+   }
+ });
+ topPage.click(function(){
+   $('body,html').animate({
+     scrollTop:0
+   },1000);
+   return false;
  });
 });
